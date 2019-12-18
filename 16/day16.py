bff7fe93794825e687c3d2ac82b011e1d25e581e
Joakim Tosteberg <joakim@tosteberg.se>
2019-12-18 20:00:19 +0100

    day16 part 2 done

--- 16/day16.py
+++ 16/day16.py
@@ -4,7 +4,7 @@ import math
 sequence = []
 with open("day16.input") as file:
     str_seq = file.read()
-    initial_sequence = numpy.matrix([int(char) for char in str_seq])
+    initial_sequence = [int(char) for char in str_seq]
 
 def get_pattern(base_pattern, position):
     pattern = []
@@ -27,8 +27,13 @@ def fft(sequence, pattern):
         row.append(abs(sequence*multiplier).item(0) % 10)
     return numpy.matrix(row)
 
+def fft_second_half(half_sequence):
+    for i in range(len(half_sequence)-2,-1,-1):
+        half_sequence[i] = (half_sequence[i] + half_sequence[i+1])%10
+    pass
+
 passes = 100
-sequence = initial_sequence
+sequence = numpy.matrix(initial_sequence)
 for i in range(0, passes):
     sequence = fft(sequence, BASE_PATTERN)
 list_seq = sequence.tolist()[0]
@@ -36,10 +41,12 @@ print(list_seq[0:8])
 
 
 offset =  int(str_seq[0:7])
-passes = 10000
-sequence = initial_sequence
-for i in range(0, passes):
-    sequence = fft(sequence, BASE_PATTERN)
+half_sequence = initial_sequence * 5000
+offset -= len(half_sequence)
+print(offset)
+print(len(half_sequence))
+passes = 100
+for i in range(passes):
+    fft_second_half(half_sequence)
 
-list_seq = sequence.tolist()[0]
-print(list_seq[offset:offset+8])
+print(half_sequence[offset:offset+8])
