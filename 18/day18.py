a2853539f7bafb7ef2c85ce655d648024c8f8a5b
Joakim Tosteberg <joakim@tosteberg.se>
2019-12-22 08:30:56 +0100

    Day 18 part 2 done

--- 18/day18.py
+++ 18/day18.py
@@ -2,17 +2,33 @@ import itertools
 cave = {}
 start_positions = []
 num_keys = 0
-with open("day18.input") as file:
-    y = 0
-    for line in file:
-        for x in range(0, len(line)):
-            cave[(x,y)] = line[x]
-            if line[x] == '@':
-                cave[(x,y)] = '.'
-                start_positions.append((x,y))
-            elif line[x].islower():
-                num_keys += 1
-        y += 1
+key_length_cache = {}
+best_steps_to_end = None
+
+def read_map(map_file):
+    global cave
+    global start_positions
+    global num_keys
+    global key_length_cache
+    global best_steps_to_end
+
+    cave = {}
+    start_positions = []
+    num_keys = 0
+    key_length_cache = {}
+    best_steps_to_end = None
+
+    with open(map_file) as file:
+        y = 0
+        for line in file:
+            for x in range(0, len(line)):
+                cave[(x,y)] = line[x]
+                if line[x] == '@':
+                    cave[(x,y)] = '.'
+                    start_positions.append((x,y))
+                elif line[x].islower():
+                    num_keys += 1
+            y += 1
 
 def is_key(tile):
     return tile.islower()
@@ -23,7 +39,6 @@ def is_door(tile):
 def is_empty(tile):
     return tile == '.'
 
-key_length_cache = {}
 def length_to_keys(cave, start_pos):
     global key_length_cache
     if start_pos in key_length_cache:
@@ -69,9 +84,7 @@ def length_to_keys(cave, start_pos):
 
     key_length_cache[start_pos] = found_keys
     return found_keys
-        
 
-best_steps_to_end = None
 
 def find_all_keys(cave, found_keys, keys, steps, total_num_keys, state_map):
     global best_steps_to_end
@@ -79,31 +92,44 @@ def find_all_keys(cave, found_keys, keys, steps, total_num_keys, state_map):
         return None
 
     if len(keys) == total_num_keys:
-        print(steps)
         return steps
 
-    for key in found_keys:
-        # Don't try to get a key we already have
-        if key in keys:
-            continue
-        # Don't try to get a key we can't reach
-        if not set(found_keys[key]['required_keys']).issubset(keys):
-            continue
-        current_keys = ''.join(sorted(keys)) + key
-        current_steps = steps + found_keys[key]['steps']
-        if current_keys in state_map and current_steps >= state_map[current_keys]:
-            continue
-        state_map[current_keys] = current_steps
-        new_keys = length_to_keys(cave, found_keys[key]['pos'])
-        steps_to_end = find_all_keys(cave, new_keys, current_keys, current_steps, total_num_keys, state_map)
-        if steps_to_end:
-            if not best_steps_to_end:
-                best_steps_to_end = steps_to_end
-            else:
-                best_steps_to_end = min(steps_to_end, best_steps_to_end)
+    for i in range(len(found_keys)):
+        for key in found_keys[i]:
+            # Don't try to get a key we already have
+            if key in keys:
+                continue
+            # Don't try to get a key we can't reach
+            if not set(found_keys[i][key]['required_keys']).issubset(keys):
+                continue
+            current_keys = ''.join(sorted(keys)) + key
+            current_steps = steps + found_keys[i][key]['steps']
+            if current_keys in state_map and current_steps >= state_map[current_keys]:
+                continue
+            state_map[current_keys] = current_steps
+            new_keys = found_keys[:]
+            new_keys[i] = length_to_keys(cave, found_keys[i][key]['pos'])
+            steps_to_end = find_all_keys(cave, new_keys, current_keys, current_steps, total_num_keys, state_map)
+            if steps_to_end:
+                if not best_steps_to_end:
+                    best_steps_to_end = steps_to_end
+                else:
+                    best_steps_to_end = min(steps_to_end, best_steps_to_end)
 
     return best_steps_to_end
 
-    
-found_keys = length_to_keys(cave, start_positions[0])
-print(find_all_keys(cave, found_keys, "", 0, num_keys, {}))
+
+def solve_map(cave, start_positions, num_keys):
+    found_keys = []
+    for start_position in start_positions:
+        found_keys.append(length_to_keys(cave, start_position))
+
+    return find_all_keys(cave, found_keys, "", 0, num_keys, {})
+
+
+read_map("day18.input")
+print("Steps for part 1=%u" % (solve_map(cave, start_positions, num_keys)))
+
+read_map("day18.part2.input")
+print("Steps for part 2=%u" % (solve_map(cave, start_positions, num_keys)))
+
