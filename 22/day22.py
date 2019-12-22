4e013df8c5681a7da298ecbead9d0e442fc73626
Joakim Tosteberg <joakim@tosteberg.se>
2019-12-22 20:08:00 +0100

    Added iterations in preparation for part 2

--- 22/day22.py
+++ 22/day22.py
@@ -1,10 +1,3 @@
-import itertools
-
-
-tracked_card_pos = 2019
-deck_size = 10007
-
-
 def deal_with_increment(tracked_card_pos, deck_size, increment):
     return (tracked_card_pos * increment) % deck_size
 
@@ -23,13 +16,25 @@ def cut(tracked_card_pos, deck_size, increment):
 def deal_into_new_stack(tracked_card_pos, deck_size):
     return  (deck_size - 1) - tracked_card_pos
 
+instructions = []
 with open("day22.input") as file:
     for line in file:
         if "deal with increment" in line:
-            tracked_card_pos = deal_with_increment(tracked_card_pos, deck_size, int(line.split(" ")[3]))
+            instructions.append(('d',int(line.split(" ")[3])))
         elif "cut" in line:
-            tracked_card_pos = cut(tracked_card_pos, deck_size, int(line.split(" ")[1]))
+            instructions.append(('c',int(line.split(" ")[1])))
         elif "deal into new stack" in line:
-            tracked_card_pos = deal_into_new_stack(tracked_card_pos, deck_size)
+            instructions.append(('s',None))
 
-print(tracked_card_pos)
+def shuffle_deck(tracked_card_pos, deck_size, iterations):
+    for i in range(iterations):
+        for instruction in instructions:
+            if instruction[0] == 'd':
+                tracked_card_pos = deal_with_increment(tracked_card_pos, deck_size, instruction[1])
+            elif instruction[0] == 'c':
+                tracked_card_pos = cut(tracked_card_pos, deck_size, instruction[1])
+            elif instruction[0] == 's':
+                tracked_card_pos = deal_into_new_stack(tracked_card_pos, deck_size)
+    return tracked_card_pos
+
+print(shuffle_deck(2019, 10007, 1))
