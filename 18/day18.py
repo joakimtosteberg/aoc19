1e0013df7889e620c5d09f19c914ceab7b8e5a8a
Joakim Tosteberg <joakim@tosteberg.se>
2019-12-22 07:50:37 +0100

    Major optimization for day 18, now solves part 1 in 6 seconds

--- 18/day18.py
+++ 18/day18.py
@@ -24,11 +24,12 @@ def is_empty(tile):
     return tile == '.'
 
 key_length_cache = {}
-def length_to_keys(cave, keys, start_pos):
+def length_to_keys(cave, start_pos):
     global key_length_cache
-    if keys in key_length_cache:
-        return key_length_cache[keys]
-    track = {start_pos: 0}
+    if start_pos in key_length_cache:
+        return key_length_cache[start_pos]
+
+    track = {start_pos: (0, [])}
     found_keys = {}
     positions = [start_pos]
     for steps in itertools.count(1):
@@ -39,28 +40,32 @@ def length_to_keys(cave, keys, start_pos):
                             pos[1] + direction[1])
 
                 # Skip positions we have reached in fewer steps
-                if next_pos in track and steps >= track[next_pos]:
+                if next_pos in track and steps >= track[next_pos][0]:
                     continue
 
                 tile = cave[next_pos]
 
-                # Don't move through doors we can't open
-                if is_door(tile) and tile.lower() not in keys:
-                    continue
-
                 # Now step onto the tile if we can
                 if is_key(tile) or is_empty(tile) or is_door(tile):
+                    required_keys = track[pos][1][:]
+
+                    # Add key requirement if it is a door
+                    if is_door(tile):
+                        required_keys += [tile.lower()]
+
+                    track[next_pos] = (steps, required_keys)
+
                     # Shortest path to key found, save it
-                    if is_key(tile) and tile not in keys:
-                        found_keys[tile] = (next_pos,steps)
-                    track[next_pos] = steps
+                    if is_key(tile):
+                        found_keys[tile] = {'pos': next_pos, 'steps': steps, 'required_keys': required_keys}
+
                     next_positions.append(next_pos)
 
         if not next_positions:
             break
         positions = next_positions
 
-    key_length_cache[keys] = found_keys
+    key_length_cache[start_pos] = found_keys
     return found_keys
         
 
@@ -76,12 +81,18 @@ def find_all_keys(cave, found_keys, keys, steps, total_num_keys, state_map):
         return steps
 
     for key in found_keys:
+        # Don't try to get a key we already have
+        if key in keys:
+            continue
+        # Don't try to get a key we can't reach
+        if not set(found_keys[key]['required_keys']).issubset(keys):
+            continue
         current_keys = ''.join(sorted(keys)) + key
-        current_steps = steps + found_keys[key][1]
+        current_steps = steps + found_keys[key]['steps']
         if current_keys in state_map and current_steps >= state_map[current_keys]:
             continue
         state_map[current_keys] = current_steps
-        new_keys = length_to_keys(cave, current_keys, found_keys[key][0])
+        new_keys = length_to_keys(cave, found_keys[key]['pos'])
         steps_to_end = find_all_keys(cave, new_keys, current_keys, current_steps, total_num_keys, state_map)
         if steps_to_end:
             if not best_steps_to_end:
@@ -92,5 +103,5 @@ def find_all_keys(cave, found_keys, keys, steps, total_num_keys, state_map):
     return best_steps_to_end
 
     
-found_keys = length_to_keys(cave, "", pos)
+found_keys = length_to_keys(cave, pos)
 print(find_all_keys(cave, found_keys, "", 0, num_keys, {}))
