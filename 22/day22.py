923026a7d8cf2556841408d72b0adefe1104a659
Joakim Tosteberg <joakim@tosteberg.se>
2019-12-23 08:55:59 +0100

    Simplified cut operation

--- 22/day22.py
+++ 22/day22.py
@@ -2,16 +2,7 @@ def deal_with_increment(tracked_card_pos, deck_size, increment):
     return (tracked_card_pos * increment) % deck_size
 
 def cut(tracked_card_pos, deck_size, increment):
-    if increment > 0:
-        if tracked_card_pos < increment:
-            return (deck_size - increment) + tracked_card_pos
-        else:
-            return tracked_card_pos - increment
-    else:
-        if tracked_card_pos < (deck_size + increment):
-            return tracked_card_pos - increment
-        else:
-            return tracked_card_pos - (deck_size + increment)
+    return tracked_card_pos - increment % deck_size
 
 def deal_into_new_stack(tracked_card_pos, deck_size):
     return  (deck_size - 1) - tracked_card_pos
