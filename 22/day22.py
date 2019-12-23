51bc3ee5086a09de7c9d362b0ef9f7a7c76d90c1
Joakim Tosteberg <joakim@tosteberg.se>
2019-12-23 10:17:23 +0100

    Solution to day 22 part 2

--- 22/day22.py
+++ 22/day22.py
@@ -52,3 +52,18 @@ def reverse_shuffle_deck(tracked_card_pos, deck_size, iterations):
     return tracked_card_pos
 
 print(shuffle_deck(2019, 10007, 1))
+
+# F(x) = A*x + B (due to all operations being linear under modulus space)
+# F(x) - F(x+1) = A*x + B - A*(x+1) + B = -A => A = F(x+1) - F(x)
+# F(x) = A*x + B => B = A*x - F(x)
+
+deck_size = 119315717514047
+iterations = 101741582076661
+F0 = reverse_shuffle_deck(2020, deck_size, 1)
+F1 = reverse_shuffle_deck(2021, deck_size, 1)
+A = F1 - F0
+B = F0 - A * 2020
+
+# F(F(F(...(F(x))))) = (A^n)*x + (A^(n-1) + A^(n-2) + ... + A + 1)*B = (A^n)*x + ((A^n-1) / (A-1)) * B mod S = (A^n)*x + ((A^n-1)*modinv(A-1,S))*B mod S
+
+print((pow(A, iterations, deck_size) * 2020 + ((pow(A, iterations, deck_size) - 1) * gmpy2.invert(A-1,deck_size)*B)) % deck_size)
