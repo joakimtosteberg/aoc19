6e8345299c60decdb38708d816224d0b42d91fd5
Joakim Tosteberg <joakim@tosteberg.se>
2019-12-24 22:01:56 +0100

    General improvements to prepare for part 2

--- 24/day24.py
+++ 24/day24.py
@@ -1,18 +1,23 @@
 import copy
 
-layout = {}
-width = 0
-height = 0
-with open("day24.input") as file:
-    y = 0
-    for line in file:
-        x = 0
-        for tile in line.rstrip():
-            layout[(x,y)] = {'bug': 1 if tile == '#' else 0, 'adjacent': 0}
-            x += 1
-            width = max(x,width)
-        y += 1
-        height = max(y,height)
+width = 5
+height = 5
+
+def update_tile(next_layout, x, y, bug_change):
+    next_layout[(x,y)]['bug'] += bug_change
+    for step in [(1,0),(-1,0),(0,1),(0,-1)]:
+        adj_x = x + step[0]
+        adj_y = y + step[1]
+        if ((adj_x,adj_y) not in next_layout):
+            continue
+        next_layout[(adj_x,adj_y)]['adjacent'] += bug_change
+
+def create_empty_layer(width, height):
+    layer = {}
+    for y in range(height):
+        for x in range(width):
+            layer[(x,y)] = {'bug': 0, 'adjacent': 0}
+    return layer
 
 def get_tile(layout, x, y):
     return layout.get((x,y), {'bug': 0,'adjacent': 0})
@@ -28,26 +33,13 @@ def get_adjacent_bugs(layout, x, y):
         adjacent_bugs += get_num_bugs(layout, adj_x, adj_y)
     return adjacent_bugs
 
-for y in range(height):
-    for x in range(width):
-        layout[(x,y)]['adjacent'] = get_adjacent_bugs(layout, x, y)
-
-def update_tile(next_layout, x, y, bug):
-    next_layout[(x,y)]['bug'] = bug
-    for step in [(1,0),(-1,0),(0,1),(0,-1)]:
-        adj_x = x + step[0]
-        adj_y = y + step[1]
-        if ((adj_x,adj_y) not in next_layout):
-            continue
-        next_layout[(adj_x,adj_y)]['adjacent'] += 1 if bug else -1
-
 def update(layout, widht, height):
     next_layout = copy.deepcopy(layout)
     for y in range(height):
         for x in range(width):
             if layout[(x,y)]['bug']:
                 if layout[(x,y)]['adjacent'] != 1:
-                    update_tile(next_layout, x, y, 0)
+                    update_tile(next_layout, x, y, -1)
             else:
                 if layout[(x,y)]['adjacent'] in [1,2]:
                     update_tile(next_layout, x, y, 1)
@@ -71,6 +63,17 @@ def calculate_biodiversity(layout, widht, height):
             if layout[(x,y)]['bug']:
                 biodiversity += pow(2,width*y + x)
     return biodiversity
+
+layout = create_empty_layer(width,height)
+
+with open("day24.input") as file:
+    y = 0
+    for line in file:
+        x = 0
+        for tile in line.rstrip():
+            update_tile(layout, x, y, 1 if tile == '#' else 0)
+            x += 1
+        y += 1
 
 layouts = [layout]
 while True:
